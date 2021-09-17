a3b5c13d3500581cb3b720b22b046490d3f126bb
polaris <axvantia@protonmail.com>
2021-09-17 17:58:09 +0100

    good css

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def base():
 
-    return render_template("main.html",)
+    return render_template("main.html.j2",)
+    
+    url_for('static', filename='main.css')
